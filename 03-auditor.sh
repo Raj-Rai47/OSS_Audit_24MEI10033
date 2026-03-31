f9002623f8950d579e416133cf88106b75d48283
Amrit Raj Rai <amritrajrai942612@gmail.com>
2026-03-31 22:22:55 +0530

    Update 03-auditor.sh

--- 03-auditor.sh
+++ 03-auditor.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
-# Author: Amrit Raj Rai (24MEI10033)
+# Author: Amrit  (24MEI10033)
+# Purpose: Disk and Permission Auditor
 
-dirs=("/etc" "/var/log" "/usr/bin" "/home" "/root/.git" "/home/$(whoami)/.git")
-
-echo "================================================================================"
-
-echo "                   Git AUDIT - DIRECTORY AUDITOR                "
+dirs=(/etc /var/log /usr/bin /home)
+dirs+=(/usr/share/git /var/lib/git)
 
 echo "================================================================================"
-for dir in "${dirs[@]}";
-    do
-        if [ -d "$dir" ]; then
-            echo "$dir exists"
-            echo "Size: $(du -sh $dir | cut -f1)"
-            echo "Permissions: $(stat -c %a $dir)"
-            echo "Owner: $(stat -c %U $dir)"
-            echo "--------------------------------------------------------------------------------"
-        else
-            echo "$dir does not exist"
-        fi
-    done
-
+for dir in "${dirs[@]}"; do
+    if [ -d "$dir" ]; then
+        echo "Directory: $dir"
+        echo "Size: $(du -sh $dir | cut -f1)"
+        echo "Permissions: $(stat -c %a $dir)"
+        echo "Owner: $(stat -c %U $dir)"
+        echo "--------------------------------------------------------------------------------"
+    else
+        echo "Directory $dir does not exist."
+    fi
+done
 echo "================================================================================"
