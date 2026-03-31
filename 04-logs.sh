1900aae2490017884c28dfa3784dbda18dcbae10
Amrit Raj Rai <amritrajrai942612@gmail.com>
2026-03-31 22:23:33 +0530

    Update 04-logs.sh

--- 04-logs.sh
+++ 04-logs.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-# Author: Amrit Raj Rai (24MEI10033)
-
+# Author: Amrit  (24MEI10033)
+# Purpose: Log File Analyzer
 # Suggested log path for Git: /var/log/git.log
 
+LOG_FILE=$1
+KEYWORD=$2
 echo "================================================================================"
-
-echo "                   Git AUDIT - LOG FILE ANALYZER                "
-
+echo "                   Git AUDIT - LOG FILE ANALYZER              "
 echo "================================================================================"
 
 count=0
-while read line;
-    do
-        if echo "$line" | grep -q "$2";
-            then
-                ((count++))
-        fi
-    done < $1
-echo "Total occurrences of '$2': $count"
-
-tail -n 5 $1 | grep "$2"
-
+while IFS= read -r line; do
+    if [[ $line == *$KEYWORD* ]]; then
+        ((count++))
+    fi
+done < $LOG_FILE
+echo "Keyword '$KEYWORD' found $count times in $LOG_FILE."
+echo "Last 5 matches:"
+tail -n 5 $LOG_FILE | grep $KEYWORD
 echo "================================================================================"
