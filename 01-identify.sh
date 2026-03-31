aa6328d6a2f5624a13b0fefe0a6d9e3911d16e82
Amrit Raj Rai <amritrajrai942612@gmail.com>
2026-03-31 22:22:03 +0530

    Update 01-identify.sh

--- 01-identify.sh
+++ 01-identify.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-# Author: Amrit Raj Rai (24MEI10033)
+# Author: Amrit  (24MEI10033)
+# Purpose: System Identity Report
 
 echo "================================================================================"
 echo "                   Git AUDIT - SYSTEM IDENTITY                   "
 echo "================================================================================"
-echo "Linux Distribution: $(cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2- | tr -d '"')"
+
+echo "Linux Distribution: $(lsb_release -ds)"
 echo "Kernel Version:     $(uname -r)"
 echo "Current User:       $(whoami)"
 echo "Home Directory:     $(pwd)"
-echo "System Uptime:      $(uptime)"
-echo "Current Date/Time:  $(date)"
+echo "System Uptime:      $(uptime -p)"
+echo "Current Date/Time:  $(date +"%a %b %d %Y %H:%M:%S %Z%z (%Z)")"
 echo "--------------------------------------------------------------------------------"
 echo "Message: This system runs on Open Source software, providing freedom to study, change, and distribute."
 echo "================================================================================"
